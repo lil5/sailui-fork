91f511154b623f15b36386cbde42cb99fd301ca4
Lucian I. Last <li@last.nl>
2021-12-03 22:57:01 +0100

    change to tabs

--- src/components/link.js
+++ src/components/link.js
@@ -1,26 +1,26 @@
-const tc = require('tinycolor2')
+const tc = require("tinycolor2");
 
-module.exports = ({theme}) => {
-    var primaryColor = theme('colors.primary')
-    var hoverColor = tc(primaryColor).clone()
+module.exports = ({ theme }) => {
+	var primaryColor = theme("colors.primary");
+	var hoverColor = tc(primaryColor).clone();
 
-    hoverColor.getBrightness() < 10 
-    ? hoverColor.lighten(20) 
-    : hoverColor.darken(10);
+	hoverColor.getBrightness() < 10
+		? hoverColor.lighten(20)
+		: hoverColor.darken(10);
 
-    hoverColor = hoverColor.toString()
+	hoverColor = hoverColor.toString();
 
-    return {
-        '.link': {
-            transitionProperty: 'color',
-            transitionDuration: '200ms',
-            cursor: 'pointer',
-            color: primaryColor,
-            textDecoration: "underline",
+	return {
+		".link": {
+			transitionProperty: "color",
+			transitionDuration: "200ms",
+			cursor: "pointer",
+			color: primaryColor,
+			textDecoration: "underline",
 
-            '&:hover': {
-                color: hoverColor
-            },
-        },
-    }
-}
+			"&:hover": {
+				color: hoverColor,
+			},
+		},
+	};
+};
--- src/sailui.js
+++ src/sailui.js
@@ -1,20 +1,26 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
-module.exports = plugin(function ({addComponents, addUtilities, addBase, theme, postcss}) {
-  let sailTheme = require('./themes/sailTheme')({theme})
+module.exports = plugin(function ({
+	addComponents,
+	addUtilities,
+	addBase,
+	theme,
+	postcss,
+}) {
+	let sailTheme = require("./themes/sailTheme")({ theme });
 
-  addComponents(require('./components/card'))
-  addComponents(require('./components/alert'))
-  addComponents(require('./components/btn')({theme: sailTheme}))
-  addComponents(require('./components/badge')({theme: sailTheme}))
-  addComponents(require('./components/link')({theme: sailTheme}))
+	addComponents(require("./components/card"));
+	addComponents(require("./components/alert"));
+	addComponents(require("./components/btn")({ theme: sailTheme }));
+	addComponents(require("./components/badge")({ theme: sailTheme }));
+	addComponents(require("./components/link")({ theme: sailTheme }));
 
-  addComponents(require('./components/form/input')({theme: sailTheme}))
-  addComponents(require('./components/form/select')({theme: sailTheme}))
-  addComponents(require('./components/form/checkbox')({theme: sailTheme}))
-  addComponents(require('./components/form/radio')({theme: sailTheme}))
-  addComponents(require('./components/form/textarea')({theme: sailTheme}))
+	addComponents(require("./components/form/input")({ theme: sailTheme }));
+	addComponents(require("./components/form/select")({ theme: sailTheme }));
+	addComponents(require("./components/form/checkbox")({ theme: sailTheme }));
+	addComponents(require("./components/form/radio")({ theme: sailTheme }));
+	addComponents(require("./components/form/textarea")({ theme: sailTheme }));
 
-  let forms = require('@tailwindcss/forms')
-  forms.handler({addBase, theme})
-})
+	let forms = require("@tailwindcss/forms");
+	forms.handler({ addBase, theme });
+});
